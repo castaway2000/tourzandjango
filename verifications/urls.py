805b7a0d3a51a95f8a72148b140866fb545e2029
AleksTerentyev <Aleks.Terentyev@gmail.com>
2017-10-25 01:04:25 +0300

    Webhook implementation, it needs some testing

--- verifications/urls.py
+++ verifications/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^identity_verification/$', views.identity_verification_router, name='identity_verification_router'),
     url(r'^identity_verification/ID_uploading/$', views.identity_verification_ID_uploading, name='identity_verification_ID_uploading'),
     url(r'^identity_verification/photo/$', views.identity_verification_photo, name='identity_verification_photo'),
+    url(r'^identity_verification/onfidowebhook/$', views.identity_verification_webhook, name='identity_verification_webhook'),
 ]
