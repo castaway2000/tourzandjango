5199f5f199964d3f782a487b0a1f0641a64faac2
AleksTerentyev <Aleks.Terentyev@gmail.com>
2017-10-27 15:18:47 +0300

    Fixing migrations

--- verifications/migrations/0012_auto_20171025_1340.py
+++ verifications/migrations/0012_auto_20171025_1340.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('verifications', '0010_webhooklog'),
+        ('verifications', '0011_webhooklog'),
     ]
 
     operations = [
