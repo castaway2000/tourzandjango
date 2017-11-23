ac3ed64ba93ef4bb01e5ec2702611e8de4fa0eba
AleksTerentyev <Aleks.Terentyev@gmail.com>
2017-11-24 00:26:38 +0200

    Categories filtering now works

--- blog/views.py
+++ blog/views.py
@@ -11,11 +11,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 
 
-def blog(request):
+def blog(request, category_slug=None):
     current_page = "blog"
     categories = BlogCategory.objects.filter(is_active=True).annotate(posts_nmb=Count("blogpost")).order_by("-posts_nmb")[:5]
     tags = BlogTag.objects.filter(is_active=True).annotate(posts_nmb=Count("blogposttag")).order_by("-posts_nmb")[:5]
-    blog_posts = BlogPost.objects.filter(is_active=True).order_by("-id").values()
+
+    kwargs = {
+        "is_active": True
+    }
+    if category_slug:
+        kwargs["category__slug"] = category_slug
+    blog_posts = BlogPost.objects.filter(**kwargs).order_by("-id").values()
 
 
     page = request.GET.get('page', 1)
@@ -31,7 +37,7 @@ def blog(request):
 
 
 def blog_post(request, slug):
-    current_page = "blog"
+    current_page = "blog_post"
     categories = BlogCategory.objects.filter(is_active=True).annotate(posts_nmb=Count("blogpost")).order_by("-posts_nmb")[:5]
     tags = BlogTag.objects.filter(is_active=True).annotate(posts_nmb=Count("blogposttag")).order_by("-posts_nmb")[:5]
     blog_post = BlogPost.objects.get(slug=slug)
