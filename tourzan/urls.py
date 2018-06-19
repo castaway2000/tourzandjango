603e685c14448eb4fae7fd8d7fff2943f019de69
Adam Szablya <aszablya@tourzan.com>
2018-06-18 23:00:39 -0700

    login, password reset, and forgot password api updates

--- tourzan/urls.py
+++ tourzan/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
-from users.api.views import login_api_view, signup_api_view
+from users.api.views import login_api_view, signup_api_view, get_jwt_user
 
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 from rest_framework.schemas import get_schema_view
@@ -79,8 +79,8 @@ urlpatterns = i18n_patterns(
                 #to access protected api urls you must include the Authorization: JWT <your_token> header.
                 #https://getblimp.github.io/django-rest-framework-jwt/
                 #http://polyglot.ninja/django-rest-framework-json-web-tokens-jwt/
-                url(r'^api/v1/api-token-auth/', obtain_jwt_token),
-                url(r'^api/v1/api-token-verify/', verify_jwt_token),
+                url(r'^api/v1/api-token-auth/', get_jwt_user),# obtain_jwt_token),
+                url(r'^api/v1/api-token-verify/', verify_jwt_token, name='verify_token'),
                 url(r'^api/v1/login_client/$', login_api_view, name='login_client'),
                 url(r'^api/v1/signup_user/$', signup_api_view, name='signup_client'),
                 url(r'^api/v1/', include('chats.api.urls')),
--- users/api/views.py
+++ users/api/views.py
@@ -5,17 +5,24 @@ from rest_framework.permissions import (
     IsAuthenticatedOrReadOnly,
     )
 
+from allauth.account.views import password_reset
 from django.contrib.auth import authenticate
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.status import HTTP_401_UNAUTHORIZED
+from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK
 from rest_framework.authtoken.models import Token
+from rest_framework_jwt.settings import api_settings
+import urllib3
+import json
 
 from rest_framework import viewsets
 from ..models import *
 from .serializers import *
 from .permissions import IsUserOwnerOrReadOnly
 
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 class InterestViewSet(viewsets.ModelViewSet):
     queryset = Interest.objects.all()
@@ -88,3 +95,76 @@ def signup_api_view(request):
 
     token, _ = Token.objects.get_or_create(user=user)
     return Response({"token": token.key})
+
+
+@api_view(['POST'])
+def get_jwt_user(request):
+    user_auth = authenticate(username=request.POST['username'], password=request.POST['password'])
+    if not user_auth:
+        return Response({"error": "Login failed"}, status=HTTP_401_UNAUTHORIZED)
+    user = User.objects.get(username=request.POST['username'])
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+    payload = jwt_payload_handler(user)
+    token = jwt_encode_handler(payload)
+
+    try:
+        guide_id = user.guideprofile.id
+    except:
+        guide_id = None
+        pass
+
+    try:
+        tourist_image = user.touristprofile.image.file
+    except ValueError:
+        tourist_image = None
+        pass
+
+
+    user_obj = {'user_id': user.id, 'username': user.username, 'email': user.email,
+                'phone': user.generalprofile.phone,
+                'building_num': user.generalprofile.registration_building_nmb,
+                'flat_num': user.generalprofile.registration_flat_nmb,
+                'street': user.generalprofile.registration_street,
+                'city': user.generalprofile.registration_city,
+                'state': user.generalprofile.registration_state,
+                'country': user.generalprofile.registration_country,
+                'postcode': user.generalprofile.registration_postcode,
+                'interests': user.userinterest_set.all(),
+                'guide_id': guide_id,
+                'profile_pic': tourist_image,
+                }
+    token_user = {"token": token, 'user': user_obj}
+    return Response(token_user)
+
+
+@api_view(['post'])
+def api_change_pass(request):
+    if request.method == 'POST':
+        tokenize = json.dumps({'token': request.POST['token']})
+        http = urllib3.PoolManager()
+        #TODO: update the url
+        req = http.request('POST', 'http://localhost:8080/validate_token',
+                           headers={'Content-Type': 'application/json'},
+                           body=tokenize)
+        if req.status != 200:
+            return HTTP_401_UNAUTHORIZED('please login')
+        user = request.user
+        form = PasswordChangeForm(data=request.POST or None, user=user)
+        if form.is_valid():
+            form.save(commit=False)
+            form.save()
+            update_session_auth_hash(request, user)
+            return HTTP_200_OK
+
+
+@api_view(['POST'])
+def forgot_password(request):
+    if request.method == 'POST':
+        email = json.dumps({'email': request.POST['email']})
+        http = urllib3.PoolManager()
+        #TODO: update the url
+        req = http.request('POST', 'http://localhost:8080/reset_password',
+                           headers={'Content-Type': 'application/json'},
+                           body=email)
+        return HttpResponse(req.status)
