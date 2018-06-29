0d2e317696adda2dbff6a4682088efd8a81a4190
Adam Szablya <aszablya@tourzan.com>
2018-06-28 22:22:56 -0700

    added quick link in settings.py for elasticache local endpoint.

--- tourzan/settings.py
+++ tourzan/settings.py
@@ -192,7 +192,7 @@ WSGI_APPLICATION = 'tourzan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test_db.sqlite3_X'),
     }
 }
 
