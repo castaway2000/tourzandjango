b6588ba4917e7b3a574943d07c87716409ef1374
Adam Szablya <aszablya@tourzan.com>
2018-07-10 01:56:33 -0700

    fixed url typo

--- mobile/api/urls.py
+++ mobile/api/urls.py
@@ -4,7 +4,7 @@ from tourzan.api_router import SharedAPIRootRouter
 
 router = SharedAPIRootRouter()
 urlpatterns = [url(r'^', include(router.urls)),
-               url(r'^get_nearby_guides', views.show_nearby_guides),
+               url(r'^get_nearby_guides/', views.show_nearby_guides),
                url(r'^get_trip_status/', views.get_trip_status),
                url(r'^update_trip/', views.update_trip),
                url(r'^extend_time/', views.extend_time),
