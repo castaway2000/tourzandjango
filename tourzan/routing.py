84c5612fa6d6b83757c34dd6a4a38a3d22f0e707
AleksTerentyev <Aleks.Terentyev@gmail.com>
2018-08-22 01:08:29 +0300

    Improvements for sockets authorization with jwt token. TBD: handle cases with proper exception messages, when jwt is incorrect or expired

--- tourzan/routing.py
+++ tourzan/routing.py
@@ -2,11 +2,14 @@ from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import live_chat.routing
+from live_chat.utils import TokenAuthMiddlewareStack
+
 import mobile.routing
+
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
+    'websocket': TokenAuthMiddlewareStack(
         URLRouter(
             live_chat.routing.websocket_urlpatterns,
         )
