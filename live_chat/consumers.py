261fa0fc23c7a2840def8c6597971805026631c2
AleksTerentyev <Aleks.Terentyev@gmail.com>
2018-08-23 14:31:51 +0300

    Chat notification implementation + all website notifications to toastr lib

--- live_chat/consumers.py
+++ live_chat/consumers.py
@@ -1,10 +1,70 @@
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncJsonWebsocketConsumer
 import json
 from chats.models import Chat, ChatMessage
 import datetime
 from django.core.cache import cache
 from tourzan.settings import USER_LASTSEEN_TIMEOUT
+from channels.layers import get_channel_layer
+
+
+class GeneralConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        """
+        Called when the websocket is handshaking as part of initial connection.
+        """
+        # Are they logged in?
+        if self.scope["user"].is_anonymous:
+            # Reject the connection
+            await self.close()
+            # await self.accept()
+        else:
+            # Accept the connection
+            await self.accept()
+
+        self.group_name = self.scope['user'].generalprofile.uuid
+
+        # Join group
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        # Send debug message for testing
+        # await self.channel_layer.group_send(
+        #     self.group_name,
+        #     {
+        #         "type": "chat.message",
+        #         "message": "test 12345",
+        #     }
+        # )
+
+    async def chat_message(self, event):
+        # Handles the "chat.message" event when it's sent to us.
+        try:
+            await self.send_json({
+                "message": "hello there 1234",
+            })
+        except Exception as e:
+            print(e)
+
+
+    async def chat_notification(self, event):
+        """
+        Send notification to user about new chat message
+        """
+        message = event["message"]
+        message_user_name = event["message_user_name"]
+        chat_uuid = event["chat_uuid"]
+        try:
+            await self.send_json({
+                "type": "new_chat_message_notification",
+                "message": message,
+                "message_user_name": message_user_name,
+                "chat_uuid": chat_uuid
+            })
+        except Exception as e:
+            print(e)
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -38,7 +98,7 @@ class ChatConsumer(WebsocketConsumer):
         self.update_last_user_activity_dt()
 
         #saving message to the database
-        message_user, dt = self.save_message_to_db(message, chat_uuid)
+        chat, message_user_name, message_to_user_obj, dt = self.save_message_to_db(message, chat_uuid)
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
@@ -46,13 +106,30 @@ class ChatConsumer(WebsocketConsumer):
             {
                 "type": "chat_message",
                 "message": message,
-                "user": message_user,
+                "user": message_user_name,
                 "dt": dt
             }
         )
 
-    # Receive message from room group
+        uuid = message_to_user_obj.generalprofile.uuid
+        layer = get_channel_layer()
+        async_to_sync(layer.group_send)(
+            uuid,
+            {
+                'type': 'chat.notification',
+                'content': 'triggered',
+                'message': message,
+                'message_user_name': message_user_name,
+                'chat_uuid': chat_uuid
+            }
+        )
+
+
+
     def chat_message(self, event):
+        """
+        Called when someone sent message to chat.
+        """
         message = event.get('message')
         user = event.get("user")
         dt = event.get("dt")
@@ -68,10 +145,12 @@ class ChatConsumer(WebsocketConsumer):
         user = self.user
         chat = Chat.objects.get(uuid=chat_uuid)
         if chat.guide == user or chat.tourist == user:
+            message_to_user_obj = chat.tourist if chat.guide == user else chat.guide #defining user to whom message was sent in chat
+
             chat_message = ChatMessage.objects.create(chat=chat, message=message, user=user)
-            message_user = user.generalprofile.first_name if hasattr(user, "generalprofile") and user.generalprofile.first_name != "" else user.username
+            message_user = user.generalprofile.get_name() if hasattr(user, "generalprofile") else user.username
             dt = chat_message.created.strftime("%m/%d/%Y %H:%M:%S")
-            return (message_user, dt)
+            return (chat, message_user, message_to_user_obj, dt)
 
     def update_last_user_activity_dt(self):
         user = self.user
