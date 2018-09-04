93a512c1c2ef4e275a43bf757478c76403c70ec3
AleksTerentyev <Aleks.Terentyev@gmail.com>
2018-09-05 00:17:17 +0300

    Blog posts improvements + relation to city and country model and page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^blog/$', views.blog, name='blog'),
-    url(r'^blog/(?P<category_slug>\S+)/$', views.blog, name='blog_category_slug'),
-    url(r'^blog_post/(?P<slug>\S+)/$', views.blog_post, name='blog_post'),
+    url(r'^blog/category/(?P<category_slug>\S+)/$', views.blog, name='blog_category_slug'),
+    url(r'^blog/(?P<slug>\S+)/$', views.blog_post, name='blog_post'),
 ]
