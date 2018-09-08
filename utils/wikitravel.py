d99ed64c776f2a33700f1292eb4e226af40d478e
Adam Szablya <aszablya@tourzan.com>
2018-09-08 15:53:05 -0700

    location description tweaks.. again for cities with issues and spacing issues

--- utils/wikitravel.py
+++ utils/wikitravel.py
@@ -5,24 +5,29 @@ import bs4
 
 def get_location_summary(location):
     try:
+        location_description = 'Description Pending'
+        location = re.sub(r'[\(].*?[\)]', '', location)  # removing parenthesis for proper search
         wiki = "https://wikitravel.org/wiki/en/api.php?action=parse&page=%s&prop=text&section=0&format=json&redirects" % location
         data = requests.get(wiki).json()
         bs = bs4.BeautifulSoup
         dataset = bs(data['parse']['text']['*'], features="html.parser").text.strip()
         stripped = re.sub(r'\n\s*\n', '\n', dataset).splitlines()
-        out = []
         if stripped[0] == location:
             stripped = stripped[1:]
-        for line in stripped:
-            if line[0] == ' ' or 'For other places with the same name,' in line or '— have a look at each of them.' in line:
+        out = []
+        for idx, line in enumerate(stripped):
+            if line.split(",")[0] == stripped[0]:
+                location_description = ' '.join(re.sub(r'\[.*?\]', '', stripped[idx]).split())
+            elif line[0] == ' ' or 'For other places with the same name,' in line or '— have a look at each of them.' in line:
                 pass
             else:
                 out.append(' '.join(re.sub(r'\[.*?\]', '', line).split()))
-        location_description = ''.join(out)
-        location_summary = ''.join(location_description.split('. '))
-        print(location_summary)
+                location_description = ''.join(out)
+        return location_description
     except KeyError:
-        location_summary = 'Description Pending'
-    return str(location_summary)
-
-get_location_summary('Tukwilla (Washington)')
+        location_description = 'Description Pending'
+        return str(location_description)
+    except Exception as err:
+        print(err)
+        location_description = 'Description Pending'
+        return str(location_description)
