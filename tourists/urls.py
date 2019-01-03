6fa8e832a6814386901078a18400638fa5897ae8
Adam Szablya <aszablya@tourzan.com>
2019-01-02 18:49:16 -0800

    adjusting SEO issues and shortening the links down for better SEO

--- tourists/urls.py
+++ tourists/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     # url(r'^profile_photos/$', views.profile_photos, name='profile_photos'),
     # url(r'^profile_photos/(?P<username>\w+)/$', views.profile_photos, name='profile_photos'),
 
-    url(r'^profile_settings/tourist/$', views.profile_settings_tourist, name='profile_settings_tourist'),
+    url(r'^profile-settings/tourist/$', views.profile_settings_tourist, name='profile_settings_tourist'),
 
 
-    url(r'^profile_settings/travel_photos/$', views.travel_photos, name='travel_photos'),
-    url(r'^profile_settings/deleting_travel_photo/(?P<photo_id>\w+)/$', views.deleting_travel_photo,
+    url(r'^profile-settings/travel-photos/$', views.travel_photos, name='travel_photos'),
+    url(r'^profile-settings/deleting-travel-photo/(?P<photo_id>\w+)/$', views.deleting_travel_photo,
         name='deleting_travel_photo'),
 
     url(r'^tourists/(?P<uuid>\S+)/overview/$', views.tourist, name='tourist'),
