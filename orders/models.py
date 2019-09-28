8210f2ab5fd1ae5811982bb521eae41a0d8a38f8
Adam Szablya <aszablya@tourzan.com>
2019-09-28 01:51:48 -0700

    fixing order message error

--- orders/models.py
+++ orders/models.py
@@ -898,6 +898,7 @@ class OrderStatusChangeHistory(models.Model):
     status = models.ForeignKey(OrderStatus)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
     def __str__(self):
         return "%s" % (self.id)
 
